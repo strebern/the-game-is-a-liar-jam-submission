61051fac512ce741e917182031bd3891d32c79db
Wydman <baptiste.int@gmail.com>
2019-08-29 18:47:07 +0200

    master : New audio prefab on character

---
+++ Assets/Scripts/Sounds/CharacterSoundsManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterSoundsManager : MonoBehaviour
+{
+    [SerializeField] private List<AudioClip> _footsteps = new List<AudioClip>();
+
+    private AudioSource _audiosource;
+
+    private void Awake()
+    {
+        _audiosource = GetComponent<AudioSource>();
+        PlayFootstepsSound();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Keypad9))
+        {
+            PlayFootstepsSound();
+        }
+    }
+
+    public void PlayFootstepsSound()
+    {
+        StartCoroutine(PlayEachFootSteps(4, 0.6f));
+    }
+
+    IEnumerator PlayEachFootSteps(int numberOfSteps, float timeBetweenSteps)
+    {
+        yield return new WaitForSeconds(Random.Range(0,0.5f));
+
+        while (numberOfSteps > 0)
+        {
+            _audiosource.clip = _footsteps[Random.Range(0, _footsteps.Count)];
+            _audiosource.pitch += Random.Range(-0.1f, 0.3f);
+            _audiosource.Play();
+            yield return new WaitForSeconds(timeBetweenSteps + Random.Range(-0.5f,0.5f));
+            _audiosource.volume += 0.05f;
+            _audiosource.pitch = 1;
+            numberOfSteps--;
+        }
+        _audiosource.volume = 0.1f;
+        _audiosource.pitch = 1;
+    }
+}
