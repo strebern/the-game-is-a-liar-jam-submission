f0f5298a0a67e2bcc96d456ff424ac3ce0078bc7
Shashimee <simon.trebern@outlook.com>
2019-08-31 16:26:06 +0200

    master : Added highlighting & fixed translation error

---
+++ Assets/Scripts/CharacterSelection/CharacterHighlighter.cs
@@ -0,0 +1,20 @@
+﻿using Game.Utils;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Game.CharacterSelection
+{
+    public class CharacterHighlighter : MonoBehaviour
+    {
+        [Header("References")]
+        [SerializeField] private Button _targetButton; 
+        
+        public void Highlight(bool b)
+        {
+            if (_targetButton.interactable)
+            {
+                GetComponentInChildren<ObjectActivator>().SetActive(b);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Utils/ObjectActivator.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace Game.Utils
+{
+    public class ObjectActivator : MonoBehaviour
+    {
+        [Header("Target")]
+        [SerializeField] private GameObject _target;
+
+        public void SetActive(bool b)
+        {
+            _target.SetActive(b);
+        }
+    }
+}
