7e54faa5ab0f00f4d2d6d0354169a9b31addb39e
Wydman <baptiste.int@gmail.com>
2019-08-31 15:35:35 +0200

    master : Difficulty system

---
+++ Assets/Scripts/Game/DifficultyManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class DifficultyManager : MonoBehaviour
+{
+    [SerializeField] private GoblinsSpawnNumber _goblinsSpawnNumber;
+    [SerializeField] private TextMeshProUGUI _goblinsNumbers;
+
+    private int _currentGoblinsSpawnNumber;
+
+    //CORE
+
+    private void Awake()
+    {
+        _currentGoblinsSpawnNumber = _goblinsSpawnNumber._goblinsBaseNumbers + Random.Range(-5,6); ;
+        _goblinsNumbers.text = "Goblins to kill : " + _currentGoblinsSpawnNumber;
+    }
+
+    //PUBLIC
+
+    public void IncreaseDifficulty()
+    {
+        _goblinsSpawnNumber._goblinsBaseNumbers += 5;
+    }
+
+    public void ResetDifficulty()
+    {
+        _goblinsSpawnNumber._goblinsBaseNumbers = 30;
+        _goblinsNumbers.text = "Goblins to kill : " + _goblinsSpawnNumber._goblinsBaseNumbers;
+    }
+}
---
+++ Assets/Scripts/Game/GoblinsSpawnNumber.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "GoblinsNumbers", menuName = "Game/GoblinsSpawnNumber", order = 1)]
+public class GoblinsSpawnNumber : ScriptableObject
+{
+    public int _goblinsBaseNumbers;
+}
